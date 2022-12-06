1e6626f52f734bb0ecd21e0e09e620de7bd3cb8e
Normonge <nmarroquin.smdc@gmail.com>
2022-12-06 14:40:18 -0500

    commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+Repository repo = new Repository();
+
+System.out.println(repo.NMSCan());
+
+
+    }
+
+}
---
+++ src/Repository.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Repository {
+
+    public String NMSCan() throws IOException {
+        File file = new File("example_graph.txt");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+
+                while((line = reader.readLine()) != null){
+                        System.out.println(line);
+                }
+        }
+
+        return null;
+    }
+}
